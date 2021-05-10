1476ec39377e84a5ef636be90cac819095e4e3da
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-05-10 20:24:49 +0530

    Specify facial features in output images

--- main.py
+++ main.py
@@ -11,5 +11,13 @@ test_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2RGB)
 face_locations = face_recognition.face_locations(img)[0]
 encode_locations = face_recognition.face_encodings(img)[0]
 
+cv2.rectangle(
+    img,
+    (face_locations[3], face_locations[0]),
+    (face_locations[1], face_locations[2]),
+    (0, 255, 0),
+    2
+)
+
 cv2.imshow('Mukesh Ambani - Train', img)
 cv2.waitKey(0)
