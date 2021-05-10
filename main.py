f1e024f6d900d0c64e1164ce8c4486d072479d90
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-05-10 20:21:33 +0530

    Load images

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import cv2
 import numpy as np
 import face_recognition
+
+
+img = face_recognition.load_image_file('data/MukeshAmbaniImage1.jpg')
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+test_img = face_recognition.load_image_file('data/MukeshAmbaniImage2.jpg')
+test_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2RGB)
+
+cv2.imshow('Mukesh Ambani - Train', img)
+cv2.waitKey(0)
