5e3b9a2a709a34c669dd3b5a4609ae9759e3bcf3
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-05-10 20:27:01 +0530

    Add face detection for test image

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import face_recognition
 
 img = face_recognition.load_image_file('data/MukeshAmbaniImage1.jpg')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-test_img = face_recognition.load_image_file('data/MukeshAmbaniImage2.jpg')
-test_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2RGB)
+img_test = face_recognition.load_image_file('data/MukeshAmbaniImage2.jpg')
+img_test = cv2.cvtColor(img_test, cv2.COLOR_BGR2RGB)
 
 face_locations = face_recognition.face_locations(img)[0]
 encode_locations = face_recognition.face_encodings(img)[0]
@@ -19,5 +19,17 @@ cv2.rectangle(
     2
 )
 
+face_locations_test = face_recognition.face_locations(img_test)[0]
+encode_locations_test = face_recognition.face_encodings(img_test)[0]
+
+cv2.rectangle(
+    img_test,
+    (face_locations_test[3], face_locations_test[0]),
+    (face_locations_test[1], face_locations_test[2]),
+    (0, 255, 0),
+    2
+)
+
 cv2.imshow('Mukesh Ambani - Train', img)
+cv2.imshow('Mukesh Ambani - Test', img_test)
 cv2.waitKey(0)
