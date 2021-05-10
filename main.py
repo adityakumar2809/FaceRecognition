0ff6b081b08fdb026336d981108fb03bf837771f
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-05-10 20:22:45 +0530

    Get locations of facial features

--- main.py
+++ main.py
@@ -8,5 +8,8 @@ img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 test_img = face_recognition.load_image_file('data/MukeshAmbaniImage2.jpg')
 test_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2RGB)
 
+face_locations = face_recognition.face_locations(img)[0]
+encode_locations = face_recognition.face_encodings(img)[0]
+
 cv2.imshow('Mukesh Ambani - Train', img)
 cv2.waitKey(0)
