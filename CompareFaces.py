f6dee6b8ec657e47dc12e9763ce2a642dc5fdfd0
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-05-10 21:47:50 +0530

    Create file for comparing faces with train data

---
+++ CompareFaces.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import cv2
+import pickle
+import numpy as np
+import pandas as pd
+import face_recognition
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
